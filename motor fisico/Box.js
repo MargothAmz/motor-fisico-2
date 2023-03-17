53d02f1b909af273f4dde4765a056d23d85ef5bc
MargothAmz <93557612+MargothAmz@users.noreply.github.com>
2023-03-17 11:42:46 -0600

    Add files via upload

---
+++ motor fisico/Box.js
@@ -0,0 +1,14 @@
+class Box
+    {
+      constructor(x,y,w,h){
+          this.x = x;
+          this.y = y;
+          this.width = w;
+          this.height = h;
+      }    
+      display(){
+          rect(this.x, this.y, this.width,this.height);
+      }
+        
+    }
+
